12e265b5cff332bac69ded63c11eb09a1702ffa1
Kapil-Bhattarai <kapil.edu.bhattarai@gmail.com>
2020-07-04 15:12:23 +0545

    Change : Color Changed

--- reactbutton/src/main/java/com/amrdeveloper/reactbutton/ReactConstants.java
+++ reactbutton/src/main/java/com/amrdeveloper/reactbutton/ReactConstants.java
@@ -10,7 +10,7 @@ final class ReactConstants {
     static final String BLUE  = "#0366d6";
     static final String RED_LOVE  = "#f0716b";
     static final String RED_ANGRY  = "#f15268";
-    static final String YELLOW_HAHA = "#fde99c";
+    static final String YELLOW_HAHA = "#f0ba15";
     static final String YELLOW_WOW = "#f0ba15";
     static final String GRAY = "#616770";
     static final String ORANGE_CARE = "#EDA83A";
