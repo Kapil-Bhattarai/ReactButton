3f2a247f292a43f6dd851c02932898f8c0b69fef
Kapil-Bhattarai <kapil.edu.bhattarai@gmail.com>
2020-07-04 12:40:41 +0545

    Demo updated

--- app/src/main/java/com/amrdeveloper/reactbuttonlibrary/MainActivity.java
+++ app/src/main/java/com/amrdeveloper/reactbuttonlibrary/MainActivity.java
@@ -2,6 +2,11 @@ package com.amrdeveloper.reactbuttonlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.amrdeveloper.reactbutton.ReactButton;
+import com.amrdeveloper.reactbutton.Reaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ReactButton reactButton = findViewById(R.id.reaction_btn);
+
+        reactButton.setReactDismissListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                Reaction currentReaction = ((ReactButton) view).getCurrentReaction();
+                Toast.makeText(MainActivity.this,currentReaction.getReactType(),Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        reactButton.setReactClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reaction currentReaction = ((ReactButton) v).getCurrentReaction();
+
+                Toast.makeText(MainActivity.this,currentReaction.getReactType(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
